68356a99b1709b8672add9491397c0c2f41197fe
lfwells <Lindsay.Wells@utas.edu.au>
2022-12-09 23:09:51 +1100

    day 2

--- day2.js
+++ day2.js
@@ -1,3 +1,30 @@
 import * as fs from 'fs';
 
 const input = fs.readFileSync("day2.input").toString();
+
+let moves = input.split("\n").map(l => l.split(" "));
+
+let actions = {
+    "forward": [1, 0, 0],
+    "down": [0, 0, 1],
+    "up": [0, 0, -1]
+}
+
+let position = [0, 0, 0];
+
+moves.forEach(move => {
+    let action = actions[move[0]];
+    let amount = move[1];
+
+    for (let i = 0; i < amount; i++) {
+        position[0] += action[0];
+        position[1] += action[1]; 
+        position[2] += action[2];   
+        if (move[0] == "forward")
+        {
+            position[1] += position[2] * action[0];
+        }  
+    }
+});
+
+console.log({position, part1:position[0]*position[1]});
