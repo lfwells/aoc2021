7c1e1c83a0ee19ccab07de7d6be4a0ba4886d595
lfwells <Lindsay.Wells@utas.edu.au>
2022-12-14 02:34:54 +1100

    added utils to boilerplate to save time

--- day15.js
+++ day15.js
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as utils from "./utils.js";
 
 const input = fs.readFileSync("day15.input").toString();
 
--- day25.js
+++ day25.js
@@ -1,3 +1,4 @@
 import * as fs from 'fs';
+import * as utils from "./utils.js";
 
 const input = fs.readFileSync("day25.input").toString();
