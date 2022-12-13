a2d86b6071805c40e6ab5f89055a1d0a424c0d8d
lfwells <Lindsay.Wells@utas.edu.au>
2022-12-14 07:06:27 +1100

    added set functions to utils

--- utils.js
+++ utils.js
@@ -14,3 +14,52 @@ export const parseInts = function(arr)
 {
     return arr.map(n => parseInt(n));
 }
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+export const isSuperset = function(set, subset) {
+    for (const elem of subset) {
+      if (!set.has(elem)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  export const union = function(setA, setB) {
+    const _union = new Set(setA);
+    for (const elem of setB) {
+      _union.add(elem);
+    }
+    return _union;
+  }
+  
+  export const intersection = function(setA, setB) {
+    const _intersection = new Set();
+    for (const elem of setB) {
+      if (setA.has(elem)) {
+        _intersection.add(elem);
+      }
+    }
+    return _intersection;
+  }
+  
+  export const symmetricDifference = function(setA, setB) {
+    const _difference = new Set(setA);
+    for (const elem of setB) {
+      if (_difference.has(elem)) {
+        _difference.delete(elem);
+      } else {
+        _difference.add(elem);
+      }
+    }
+    return _difference;
+  }
+  
+  export const difference = function(setA, setB) {
+    const _difference = new Set(setA);
+    for (const elem of setB) {
+      _difference.delete(elem);
+    }
+    return _difference;
+  }
